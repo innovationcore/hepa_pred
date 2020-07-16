008c81137a1f503798f8a5dfd1663eb6e506f4c3
Caylin Hickey <caylin.hickey@uky.edu>
2020-07-16 12:09:55 -0400

    Adding simple API server and page

---
+++ Caylin/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
+import pickle
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return render_template('test.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    ret = { 'success': False, }
+    data = request.get_json()
+    try:
+        with open('model.pkl', 'rb') as f:
+            clr = pickle.load(f)
+            ret['results'] = clr.predict(data).tolist()
+            ret['success'] = True
+    except Exception as e:
+        ret['error'] = str(e)
+    return jsonify(ret)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
